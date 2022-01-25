466d85c1b23d325b746242a58758c601a520964c
Mathunes <matheusantunes720@gmail.com>
2022-01-25 10:02:34 -0300

    add create user method

--- App/models/UserModel.php
+++ App/models/UserModel.php
@@ -4,9 +4,29 @@ use App\Core\Connection;
 
 class UserModel extends Connection {
 
-    public function create($user) {
+    public function create(User $user) {
 
-        
+        try {
+
+            $sql = "INSERT INTO user(name, email, senha) VALUES (?, ?, ?)";
+
+            $conn = UserModel::getConnection();
+
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindValue(1, $user->getName());
+            $stmt->bindValue(2, $user->getEmail());
+            $stmt->bindValue(3, $user->getPassword());
+
+            $stmt->execute();
+
+            $conn = null;
+
+        } catch (PDOException $e) {
+
+            die('query fail: ' . $e->getMessage());
+
+        }
 
     }
 
