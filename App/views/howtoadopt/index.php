c505a226c8114e9eaef0d9cabc4a9ba4c34ea688
Mathunes <matheusantunes720@gmail.com>
2022-01-31 11:12:26 -0300

    add input whatsapp number

--- App/views/howtoadopt/index.php
+++ App/views/howtoadopt/index.php
@@ -198,6 +198,11 @@
                 <textarea class="form-control" name="message" id="form-message" rows="3"></textarea>
             </div>
 
+            <div class="mb-3">
+                <label for="whatsapp" class="form-label">WhatsApp para resposta</label>
+                <input type="text" class="form-control" id="whatsapp" placeholder="(21) 98765-4321">
+            </div>
+
             <button type="submit" class="btn">Enviar</button>
 
         </form>
@@ -214,8 +219,12 @@
 
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="<?= URL_JS . 'jquery-3.4.1.min.js' ?>"></script>
+<script src="<?= URL_JS . 'jquery.mask.min.js' ?>"></script>
 
 <script>
+        $('#whatsapp').mask('(00) 00000-0000');
+
         var myCarousel = document.querySelector('#carousel-home')
         var carousel = new bootstrap.Carousel(myCarousel, {
             interval: 2000,
